f99a8bbed38259b0fa392808260dd38a0be4c737
exxe <exxe06@MarcTerencesMBP.fritz.box>
2017-10-17 17:41:25 +0200

    Sign in ui custom classes complete

---
+++ DevslopesSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevslopesSocial
+//
+//  Created by exxe on 17/10/2017.
+//  Copyright © 2017 exxe. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GREY: CGFloat = 120.0 / 255.0
