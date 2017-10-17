663ec4e98841180353ee51fd23da239e16fb08fa
exxe <exxe06@MarcTerencesMBP.fritz.box>
2017-10-17 13:54:33 +0200

    settup finished firebasefirebase

--- DevslopesSocial/SignInVC.swift
+++ DevslopesSocial/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  DevslopesSocial
 //
 //  Created by exxe on 16/10/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
