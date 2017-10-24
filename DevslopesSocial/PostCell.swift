7b134d5962d7de4492074da0a1dac2ca6f6f6561
exxe <exxe06@MarcTerencesMBP.fritz.box>
2017-10-24 18:29:29 +0200

    finished feed-TableView

---
+++ DevslopesSocial/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  DevslopesSocial
+//
+//  Created by exxe on 24/10/2017.
+//  Copyright © 2017 exxe. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+
+}
